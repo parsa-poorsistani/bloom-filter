66bcd9ba780c2bcd47d31c42d565f2694eedde86
Parsa Poorsistani <poorsistani13@gmail.com>
2024-09-21 20:11:54 +0330

    adding SHA2 to hashing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use sha2::{Digest, Sha256};
+
 pub struct BloomFilter {
     bit_array: Vec<bool>,
     num_hashes: usize,
@@ -13,8 +15,25 @@ impl BloomFilter {
         }
     }
 
+    // Creating Multiple Hashes with one hash function
     fn hash(&self, item: &str, i: usize) -> usize {
-        return 1;
+        // Convert the first 8 bytes of the hash to a usize and modulo it by the bit array size
+        // Ex. for "foo"
+        // 1. SHA256("foo") = X
+        // 2. i = 0 as byte -> [0,0,0,0,0,0,0,0]
+        // 3. SHA256("foo" + [0,0,0,0,0,0,0,0]) = e02aa5a0b4e8a3644f8e9c10459dfb64609c95c91fe49328d228f3f10636c2ec
+        // 4. Take first 8 bytes: e02aa5a0b4e8a364 as byte -> [224, 42, 165, 160, 180, 232, 163, 100]
+        // 5. usize::from_le_bytes([224, 42, 165, 160, 180, 232, 163, 100]) = 7235236067926870112
+        // 6. return 7235236067926870112 % 1000 = 112
+
+        let mut hasher = Sha256::new();
+        hasher.update(item.as_bytes());
+        hasher.update(i.to_le_bytes());
+        let hash_res = hasher.finalize();
+
+        let mut hash_val = [0u8; 8];
+        hash_val.copy_from_slice(&hash_res[0..8]); // Take the first 8 bytes of the hash
+        usize::from_le_bytes(hash_val) % self.size
     }
 
     pub fn set(&mut self, item: &str) {
